6519e70c732a58c2cca7e2edeeab12083e5cfcd9
Yaroslav Himko <yaroslav.himko@gmail.com>
2021-04-29 00:05:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiftPagination
---
+++ SwiftPagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPagination
+//
+//  Created by Yaroslav Himko on 29.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
